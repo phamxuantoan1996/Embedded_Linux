8bf213cc24d680c4db2366094f60be5f40e9108e
Demo User <debian@beaglebone.localdomain>
2025-07-26 09:05:15 +0000

    callback argument.

--- kernel_module_extend.c
+++ kernel_module_extend.c
@@ -13,11 +13,34 @@
 
 int valueETX,arr_valueETX[5]; /*create variable and array for passing argument*/
 char *nameETX;
+int cb_valueETX = 0;
 
 /*use  macro passing argument*/
 module_param(valueETX,int,S_IRUSR|S_IWUSR);
 module_param(nameETX,charp,S_IRUSR|S_IWUSR);
 module_param_array(arr_valueETX,int,NULL,S_IRUSR|S_IWUSR);
+
+
+/*definition a handler for callback*/
+int notify_param(const char *val,const struct kernel_param *kp)
+{
+        int res = param_set_int(val,kp);
+        if(res == 0)
+        {
+                printk(KERN_INFO "Callback function called ...\n");
+                printk(KERN_INFO "New value of cb_valueETX = %d\n",cb_valueETX);
+                return 0;
+        }
+        return -1;
+}
+/*register struct for callback*/
+const struct kernel_param_ops my_param_ops =
+{
+        .set = &notify_param, //use customize setter
+        .get = &param_get_int,//use standard getter
+};
+module_param_cb(cb_valueETX,&my_param_ops,&cb_valueETX,S_IRUSR|S_IWUSR);
+
  
 /*
 ** Module Init function
@@ -56,4 +79,8 @@ MODULE_VERSION("1:1.0");
 Note : To pass argument when load module by using command below:
 -> sudo insmod kernel_module_extend.ko valueETX=14 nameETX='"pham xuan toan"' arr_valueETX=1,2,3,4,5
 
+
+Note : To change value of variable when moudule is running by using command below:
+
+-> sudo echo 1 > /sys/module/hello_world_module/parameters/cb_valueETX
 */
