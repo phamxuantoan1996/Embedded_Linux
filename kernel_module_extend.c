e7c472afa3361d22a1f4f22bf3d1c026086cd159
Demo User <debian@beaglebone.localdomain>
2025-07-27 04:47:47 +0000

    creating kernel module with file operations

--- kernel_module_extend.c
+++ kernel_module_extend.c
@@ -1,7 +1,7 @@
 /******************************************************************************
 *  \file       driver.c
 *
-*  \details    Automatically Creating a Device file
+*  \details    Creating Kernel Module with File Operations
 *
 *  \author     PhamToan
 *
@@ -9,13 +9,67 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/module.h>
+
 #include <linux/kdev_t.h>
 #include <linux/fs.h>
-#include <linux/err.h>
 #include <linux/device.h>
- 
+#include <linux/cdev.h>
+
+#include <linux/err.h>
+
+/*create device file*/ 
 dev_t dev = 0;
 static struct class *dev_class;
+static struct cdev etx_cdev;
+
+/*function prototypes of file operations*/
+static int      etx_open(struct inode *inode, struct file *file);
+static int      etx_release(struct inode *inode, struct file *file);
+static ssize_t  etx_read(struct file *filp, char __user *buf, size_t len,loff_t * off);
+static ssize_t  etx_write(struct file *filp, const char *buf, size_t len, loff_t * off);
+
+/*create file operation structure*/
+static struct file_operations fops = 
+{
+	.owner		= THIS_MODULE,
+	.read		= etx_read,
+	.write		= etx_write,
+	.open		= etx_open,
+	.release	= etx_release,
+};
+
+/*
+** This function will be called when we open the Device file
+*/
+static int etx_open(struct inode *inode, struct file *file)
+{
+        pr_info("Driver Open Function Called...!!!\n");
+        return 0;
+}
+/*
+** This function will be called when we close the Device file
+*/
+static int etx_release(struct inode *inode, struct file *file)
+{
+        pr_info("Driver Release Function Called...!!!\n");
+        return 0;
+}
+/*
+** This function will be called when we read the Device file
+*/
+static ssize_t etx_read(struct file *filp, char __user *buf, size_t len, loff_t *off)
+{
+        pr_info("Driver Read Function Called...!!!\n");
+        return 0;
+}
+/*
+** This function will be called when we write the Device file
+*/
+static ssize_t etx_write(struct file *filp, const char __user *buf, size_t len, loff_t *off)
+{
+        pr_info("Driver Write Function Called...!!!\n");
+        return len;
+}
 
 
 /*
@@ -25,21 +79,30 @@ static int __init kernel_module_extend_init(void)
 {
         /*Allocating Major number*/
         if((alloc_chrdev_region(&dev, 0, 1, "etx_dev")) <0){
-                pr_err("Cannot allocate major number for device\n");
+                pr_err("Cannot allocate major number for device.\n");
                 return -1;
         }
-        pr_info("Major = %d Minor = %d \n",MAJOR(dev), MINOR(dev));
- 
+        pr_info("Major = %d Minor = %d.\n",MAJOR(dev), MINOR(dev));
+
+	/*Creating cdev structure*/
+	cdev_init(&etx_cdev,&fops);
+
+	/*adding character device to system*/
+	if((cdev_add(&etx_cdev,dev,1)) < 0)
+	{
+		pr_err("Cannot add the device to the system.\n");
+	}
+
         /*Creating struct class*/
         dev_class = class_create(THIS_MODULE,"etx_class");
         if(IS_ERR(dev_class)){
-            pr_err("Cannot create the struct class for device\n");
+            pr_err("Cannot create the struct class for device.\n");
             goto r_class;
         }
  
         /*Creating device*/
         if(IS_ERR(device_create(dev_class,NULL,dev,NULL,"etx_device"))){
-            pr_err("Cannot create the Device\n");
+            pr_err("Cannot create the Device.\n");
             goto r_device;
         }
         pr_info("Kernel Module Inserted Successfully...\n");
@@ -60,6 +123,7 @@ static void __exit kernel_module_extend_exit(void)
 {
         device_destroy(dev_class,dev);
         class_destroy(dev_class);
+	cdev_del(&etx_cdev);
         unregister_chrdev_region(dev, 1);
         pr_info("Kernel Module Removed Successfully...\n");
 }
@@ -69,7 +133,7 @@ module_exit(kernel_module_extend_exit);
  
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Pham Xuan Toan");
-MODULE_DESCRIPTION("Automatically Creating a Device file");
+MODULE_DESCRIPTION("Kernel Module with File Operations");
 MODULE_VERSION("1.2");
 
 
